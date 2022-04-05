09aee7c1406c64207d26c469a733c34ebdbadce4
Ahmet Kaya <ahka1143@gmail.com>
2022-04-05 15:24:21 +0300

    05.04.2022

---
+++ src/_14_Encapsulation/Q01/Araba.java
@@ -0,0 +1,21 @@
+package _14_Encapsulation.Q01;
+
+public class Araba {
+
+
+    private String model;
+    private String renk;
+    private int motor;
+    private int yil;
+
+    public Araba(String model, String renk, int motor, int yil) {
+        this.model = model;
+        this.renk = renk;
+        this.motor = motor;
+        this.yil = yil;
+    }
+
+    public Araba() {
+
+    }
+}
---
+++ src/_14_Encapsulation/Q01/ArabaMain.java
@@ -0,0 +1,9 @@
+package _14_Encapsulation.Q01;
+
+public class ArabaMain {
+    public static void main(String[] args) {
+        Araba arb1=new Araba("Volvo S90","Siyah",120,2020);
+        Araba arb2=new Araba("Toyota Corolla","Lacivert",100,2022);
+        Araba arb3=new Araba();
+    }
+}
