8ec7a4906f08374973ac6b310682320200770add
Ahmet Kaya <ahka1143@gmail.com>
2022-04-07 07:26:29 +0300

    07.04.2022

--- src/P05_HalukMarket/Methods.java
+++ src/P05_HalukMarket/Methods.java
@@ -14,10 +14,25 @@ public class Methods extends Variables {
         System.out.println("Lutfen kac kg almak isteginizi giriniz: ");
         double miktar = scan.nextDouble();
 
-        sepet.add(miktar+" kg "+(urunListesi.get(secilenUrun - 1).replaceAll("\\d",""))+" ");
-        sepetTutari+=fiyatListesi.get(secilenUrun-1)*miktar;
-        System.out.println("Su an sepetinizdeki urunler: " + sepet + "\nBaska bir urun almak ister misiniz? E/H");
+        sepet.add(miktar + " kg " + (urunListesi.get(secilenUrun - 1).replaceAll("\\d", "")) + " ");
+        sepetTutari += fiyatListesi.get(secilenUrun - 1) * miktar;
+        System.out.println("Su an sepetinizdeki urunler: " + sepet);
+        secimYap();
 
+
+    }
+
+    protected static double paraUstuHesapla() {
+
+        System.out.println("Lutfen hesap bakiyenizi giriniz:");
+        musteriBakiye = scan.nextDouble();
+        System.out.println("Hesap bakiyeniz : " + musteriBakiye + "$ Odemeniz gereken tutar: " + (float) sepetTutari + " $");
+        System.out.println("Para ustunuz: " + (float) (musteriBakiye - sepetTutari) + " $'dir.\nBizi tercih ettiginiz icin tesekkurler. Yine bekleriz.");
+        return (musteriBakiye - sepetTutari);
+    }
+
+    protected static void secimYap() {
+        System.out.println("Baska bir urun almak ister misiniz? E/H");
         String secim = scan.next().substring(0, 1).toUpperCase();
 
         if (secim.equals("E")) {
@@ -25,18 +40,11 @@ public class Methods extends Variables {
         } else if (secim.equals("H")) {
             paraUstuHesapla();
 
+        } else {
+            System.out.println("Hatali secim yaptiniz. Lutfen tekrar deneyiniz");
+            secimYap();
         }
-
-
     }
-
-    protected static double paraUstuHesapla(){
-
-        System.out.println("Lutfen hesap bakiyenizi giriniz:");
-        musteriBakiye=scan.nextDouble();
-        System.out.println("Hesap bakiyeniz : "+musteriBakiye+ " Odemeniz gereken tutar: "+(float) sepetTutari+" $");
-        System.out.println("Para ustunuz: "+ (float)(musteriBakiye-sepetTutari)+" $'dir.\nBizi tercih ettiginiz icin tesekkurler. Yine bekleriz.");
-    return (musteriBakiye-sepetTutari);}
 
 
 }
