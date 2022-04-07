7d675f31c35a48e71f60b1926d3d3c64c94f86e4
Ahmet Kaya <ahka1143@gmail.com>
2022-04-08 01:19:42 +0300

    08.04.2022

---
+++ src/P06_KahveMakinesi/Kahve_mqkinesi01/KahveRunner.java
@@ -0,0 +1,7 @@
+package P06_KahveMakinesi.Kahve_mqkinesi01;
+
+public class KahveRunner extends HangiKahve{
+    public static void main(String[] args) throws InterruptedException {
+        giris();
+    }
+}
